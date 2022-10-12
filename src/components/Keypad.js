d96b932c5519b7ef417cc825aca461553f02fdbd
Johnson <johnson.nganga@student.moringaschool.com>
2022-10-12 22:55:30 +0300

    Task

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
-// Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+
+    function handlePassword(){
+        console.log("Entering password...")
+    }
+
     return (
-        <div></div>
+        <div type="password" onChange={handlePassword} />
     )
 }
 
